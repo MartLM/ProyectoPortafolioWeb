1e9081e1a16c6d2e2b579269395aef6f4c62dcf2
MartLM <52709176+MartLM@users.noreply.github.com>
2021-10-31 00:52:29 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const ham = document.querySelector(".ham");
 const enlaces = document.querySelector(".menu");
-const menu = document.querySelector(".list")
-
+const menu = document.querySelector(".list");
+const contc = document.querySelector(".ancla4");
 
 ham.addEventListener('click', () =>{
     enlaces.classList.toggle('move');
     menu.classList.toggle('op');
 })
+
